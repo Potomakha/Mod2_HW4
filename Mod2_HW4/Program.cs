53e8fd0c3d8b211aeb9ae0d7ea807045bbd65df9
Oleh Potomakha <potomakha@gmail.com>
2021-11-22 12:42:43 +0200

    Добавьте файлы проекта.

---
+++ Mod2_HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod2_HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
